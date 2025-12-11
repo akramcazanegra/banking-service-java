822e6e9b279ea31d7802558612c0fe0fc8482ec8
Akramcazanegra <142438950+akramcazanegra@users.noreply.github.com>
2025-12-12 00:55:53 +0100

    Refactor Transaction constructor and toString method

--- Transaction.java
+++ Transaction.java
@@ -4,7 +4,6 @@ public class Transaction {
     private int balance;
 
     public Transaction(String date, int amount, int balance) {
-        // Tqadit l'date bach twelli DD/MM/YYYY kima f l'énoncé
         this.date = date.replace("-", "/"); 
         this.amount = amount;
         this.balance = balance;
@@ -12,8 +11,6 @@ public class Transaction {
 
     @Override
     public String toString() {
-        // Kheddit String.format bach ykoun l'affichage mqad w ybann kima f l'énoncé
-        // "%-10s" l'Date, "%-6d" l'Amount, w "%d" l'Balance
         return String.format("%-10s || %-6d || %d", date, amount, balance);
     }
 }
